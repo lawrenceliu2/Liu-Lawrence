cbf8d63330793f51ba1c9b1bd4d18f5f8af62624
Lawrence Liu <lawrence255@yahoo.com>
2016-09-26 14:04:22 -0400

    why wont it worrkkkk~~

--- util/testmod.py
+++ util/testmod.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-import util.testmod
+import random
 
-from flask import Flask, render_template
-app = Flask(__name__)
-'''
 def randjob():
     #store the data from the csv into a list, s
     a = open ('occupations.csv', 'r')
@@ -33,20 +30,6 @@ def weightedrand(dict):
             return job
         else:
             start = stop
-'''
-@app.route("/")
-def hello_world():
-    return "No hablo queso"
 
-@app.route("/occupations")
-def jobs():
-    joblist = (util.testmod.randjob()).items()
-    return render_template("Occupations.html", jobs = "Occupations", diction = joblist, rand = util.testmod.weightedrand(util.testmod.randjob()))
-
-@app.route("/test")
-def testing():
-    return util.testmod.goo()
-
-
-if __name__=="__main__":
-    app.run()
+def goo():
+    return "Testing out stuff, I'm tired."
